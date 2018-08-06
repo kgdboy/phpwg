ea9660788ee3de1df1aaa11deae6e0f3aaa77090
kgdboy <kgdboy@qq.com>
2018-08-06 23:36:44 +0800

    2336

--- wg.js
+++ wg.js
@@ -2,9 +2,10 @@
 * @Author: MARK_W
 * @Date:   2018-08-06 15:05:48
 * @Last Modified by:   MARK_W
-* @Last Modified time: 2018-08-06 22:21:23
+* @Last Modified time: 2018-08-06 23:36:04
 */
 
 window.onload= function (argument) {
     let i =1;
+    let k =0;
 }
