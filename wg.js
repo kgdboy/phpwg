77b985f457723bcb60d0ea91b936ba1d109c396c
kgdboy <kgdboy@qq.com>
2018-08-06 21:23:41 +0800

    last

--- wg.js
+++ wg.js
@@ -2,9 +2,9 @@
 * @Author: MARK_W
 * @Date:   2018-08-06 15:05:48
 * @Last Modified by:   MARK_W
-* @Last Modified time: 2018-08-06 15:06:17
+* @Last Modified time: 2018-08-06 21:23:18
 */
 
 window.onload= function (argument) {
-    alert(4);
+
 }
