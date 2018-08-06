802dea1f5b5fa7ea150a9037edb1c77998a2ed2a
kgdboy <kgdboy@qq.com>
2018-08-06 16:53:12 +0800

    2018/08/06 wg commit two files

---
+++ wg.js
@@ -0,0 +1,10 @@
+/*
+* @Author: MARK_W
+* @Date:   2018-08-06 15:05:48
+* @Last Modified by:   MARK_W
+* @Last Modified time: 2018-08-06 15:06:17
+*/
+
+window.onload= function (argument) {
+    alert(4);
+}
