7f0f067887f954e3587597ab0f3db2326c251c39
kgdboy <kgdboy@qq.com>
2018-08-06 22:23:56 +0800

    change wg.js and wg.html

--- wg.js
+++ wg.js
@@ -2,9 +2,9 @@
 * @Author: MARK_W
 * @Date:   2018-08-06 15:05:48
 * @Last Modified by:   MARK_W
-* @Last Modified time: 2018-08-06 21:23:18
+* @Last Modified time: 2018-08-06 22:21:23
 */
 
 window.onload= function (argument) {
-
+    let i =1;
 }
